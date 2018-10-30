cf84ae1e32a64f3f88a0881946aa1c83f95130ad
Sam Atkins <samatkins@outlook.com>
2018-10-30 21:05:05 +0000

    add command to seed dev db

--- services/users/manage.py
+++ services/users/manage.py
@@ -3,6 +3,7 @@ import unittest
 from flask.cli import FlaskGroup
 
 from src import create_app, db
+from src.api.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -12,6 +13,14 @@ cli = FlaskGroup(create_app=create_app)
 def recreate_db():
     db.drop_all()
     db.create_all()
+    db.session.commit()
+
+
+@cli.command()
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='sam', email="sam@example.com"))
+    db.session.add(User(username='sammi', email="sammi@example.com"))
     db.session.commit()
 
 
