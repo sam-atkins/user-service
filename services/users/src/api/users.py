b98ab517904b6704efe5a0a0bdc76f28fbcd489d
Sam Atkins <samatkins@outlook.com>
2018-10-30 20:43:42 +0000

    endpoint to add a user

--- services/users/src/api/users.py
+++ services/users/src/api/users.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from sqlalchemy import exc
+
+from src.api.models import User
+from src import db
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -6,3 +10,27 @@ users_blueprint = Blueprint('users', __name__)
 @users_blueprint.route('/users/ping', methods=['GET'])
 def health_check():
     return jsonify({'status': 'success', 'message': 'ping!'})
+
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+    response_object = {'status': 'fail', 'message': 'Invalid payload.'}
+    if not post_data:
+        return jsonify(response_object), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_object['status'] = 'success'
+            response_object['message'] = f'{email} was added!'
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That email already exists.'
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
--- services/users/src/tests/test_users.py
+++ services/users/src/tests/test_users.py
@@ -15,6 +15,76 @@ class TestUserService(BaseTestCase):
         self.assertIn('ping!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        """Ensure a new user can be added to the db."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'sam',
+                    'email': 'sam@example.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('sam@example.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_add_user_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        """
+        Ensure error is thrown if the JSON object does not have a username key.
+        """
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'email': 'sam@example.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_email(self):
+        """Ensure error is thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'sam',
+                    'email': 'sam@example.com'
+                }),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'sam',
+                    'email': 'sam@example.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry. That email already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+
 
 if __name__ == '__main__':
     unittest.main()
